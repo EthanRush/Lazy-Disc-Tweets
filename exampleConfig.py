c26f23f460c6e48f12eb082db18406f2bc3e9dd7
unknown <rush.e@husky.neu.edu>
2020-06-04 18:55:36 -0500

    Added example config file and tweaked readme

---
+++ exampleConfig.py
@@ -0,0 +1,7 @@
+# If replicating this project, either rename this file to config.py or change the imports to exampleConfig
+# include config.py in your .gitignore
+clientToken = 'Your discord client token here'
+twitterConsumerKey = 'Your consumer key here'
+twitterConsumeSecret = 'Your consumer secret here'
+twitterAccessTokenKey = 'Your twitter access token Key here'
+twitterAccessTokenSecret = 'Your twitter access token secret here'
