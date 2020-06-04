49fc44f0dbfbcbc5acaa753673a0a88713f693c9
unknown <rush.e@husky.neu.edu>
2020-06-03 23:19:26 -0500

    Testing .gitignore

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # include config.py in your .gitignore
-clientToken = 'Your client token here'
+clientToken = 'TESTING GITIGNORE'
 twitterConsumerKey = 'Your consumer key here'
 twitterConsumeSecret = 'Your consumer secret here'
 twitterAccessTokenKey = 'Your twitter access token Key here'
