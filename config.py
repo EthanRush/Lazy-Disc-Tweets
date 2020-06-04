e130d32cdbea2135928ae1722e075c685aa12bf9
unknown <rush.e@husky.neu.edu>
2020-06-03 23:08:02 -0500

    Added config file

---
+++ config.py
@@ -0,0 +1,6 @@
+# include config.py in your .gitignore
+clientToken = 'Your client token here'
+twitterConsumerKey = 'Your consumer key here'
+twitterConsumeSecret = 'Your consumer secret here'
+twitterAccessTokenKey = 'Your twitter access token Key here'
+twitterAccessTokenSecret = 'Your twitter access token secret here'
