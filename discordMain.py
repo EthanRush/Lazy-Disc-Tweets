cf3262c1e2a4b698c66d3994cd279edc3505b444
unknown <rush.e@husky.neu.edu>
2020-06-07 03:16:10 -0500

    Slight tweaks and clean up of code

--- discordMain.py
+++ discordMain.py
@@ -4,23 +4,50 @@ import asyncio
 
 client = discord.Client()
 
+picExt = ['jfif', 'pjepg', 'jpeg', 'pjp', 'jpg', 'png', 'webp']
+videoExt = ['gif', 'm4v', 'mp4', 'mov', 'webm']
+
+
+
+# obtains the URL for a discord attachment
+def getUrl(msg):
+    return msg.attachments[0].url
+
+# obtains the file extension type given a url
+def getExtension(url):
+    return url.rsplit('.', maxsplit = 1)[1]
+
+
+# checks if a message is a discord attachment
+def isDiscordAttachment(msg):
+    return len(msg.attachments) > 0 
+
+def isValidAttachment(msg, extList):
+    return getExtension(getUrl(msg)).lower() in extList
+
+
+
+
 @client.event
 async def on_ready():
     print('{.user} stuck the superhero landing'.format(client))
 
 @client.event
 async def on_message(message):
+    
+    #checks if the message is from the original message
+    def isOp(msg):
+        return msg.author == message.author
+    
     if message.author == client.user:
         return
 
     if message.content.startswith('!postTextTweet'):
         await message.channel.send('Please type out the text for the tweet you want to post')
         
-        def statusCheck(msg1):
-            return msg1.author == message.author
-
+       
         try:
-            msg1 = await client.wait_for('message', timeout=60.0, check=statusCheck)
+            msg1 = await client.wait_for('message', timeout=60.0, check=isOp)
 
         except asyncio.TimeoutError:
             await message.channel.send("Timeout, tweet has been terminated")
@@ -33,11 +60,8 @@ async def on_message(message):
     if message.content.startswith('!postPhotoTweet'):
         await message.channel.send('Please type out the text for the tweet you want to post')
 
-        def statusCheck(msg1):
-            return msg1.author == message.author
-
         try:
-            msg1 = await client.wait_for('message', timeout=60.0, check=statusCheck)
+            msg1 = await client.wait_for('message', timeout=60.0, check=isOp)
 
         except asyncio.TimeoutError:
             await message.channel.send("Timeout, tweet has been terminated")
@@ -63,12 +87,12 @@ async def on_message(message):
 
                 picsLeft = int(msg2.content)
                 picIds = []
-                await message.channel.send('Please send {0} photos, uploaded as seperate discord attachments'.format(picsLeft))
+                await message.channel.send('Please send {0} photo(s), uploaded as seperate discord attachments'.format(picsLeft))
 
-                def mediaCheck(msg3):
-                    return msg3.author == message.author #and msg3.content.startswith('https://cdn.discordapp.com/attachments/')
-
-                for i in range(picsLeft, 0, -1):
+                def mediaCheck(msg):
+                    return isOp(msg) and isDiscordAttachment(msg) and isValidAttachment(msg, picExt)
+                    
+                while picsLeft > 0:
                     try:
                         msg3 = await client.wait_for('message', timeout=60.0, check=mediaCheck)
 
@@ -76,8 +100,10 @@ async def on_message(message):
                         await message.channel.send("Timeout, tweet has been terminated")
 
                     else:
-                        print(msg3.content) #TO DO FIGURE OUT HOW TO OBTAIN LINK FROM IMAGES (DON'T SHOW UP UNDER MESSAGE.CONTENT)
-                        picIds.append('') #TODO pass twitter bot the message.content and return the id with upload_media()
+                        picsLeft -= 1
+                        print(msg3.attachments[0].url) #TO DO FIGURE OUT HOW TO OBTAIN LINK FROM IMAGES (DON'T SHOW UP UNDER MESSAGE.CONTENT)
+                        #TODO pass twitter bot the message.content and return the id with upload_media()
+                        picIds.append('') 
 
                 # have twitterBot use update_status(status= tempText, media_ids=","".join(picIds))
                 # NOTE depending on how IDs are returned may need to force them to be strings in the list
@@ -88,11 +114,8 @@ async def on_message(message):
     if message.content.startswith('!postVideoTweet'):
         await message.channel.send('Please type out the text for the tweet you want to post')
 
-        def statusCheck(msg1):
-            return msg1.author == message.author
-
         try:
-            msg1 = await client.wait_for('message', timeout=60.0, check=statusCheck)
+            msg1 = await client.wait_for('message', timeout=60.0, check=isOp)
 
         except asyncio.TimeoutError:
             await message.channel.send("Timeout, tweet has been terminated")
@@ -101,11 +124,11 @@ async def on_message(message):
             statusText = msg1.content
             await message.channel.send('Please send the video as a discord attachment')
 
-            def mediaCheck(msg2):
-                 return p.user == message.author and p.content.startswith('https://cdn.discordapp.com/attachments/')
+            def videoCheck(msg):
+                return isOp(msg) and isDiscordAttachment(msg) and isValidAttachment(msg, videoExt)
 
             try:
-                msg2 = await client.wait_for('message', timeout=60.0, check=mediaCheck)
+                msg2 = await client.wait_for('message', timeout=60.0, check=videoCheck)
 
             except asyncio.TimeoutError:
                 await message.channel.send("Timeout, tweet has been terminated")
